10b59ade1e4f9c8c7777a6c1b5223dade4e54655
CORNELIUS OMOWAIYE <comowaiye@gmail.com>
2020-01-15 18:15:16 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -27,6 +27,7 @@ def directors_totals(nds)
   director_index = 0 
   while director_index < nds.size do
     director = nds[director_index]
-    result[director[:name]] = gross_for_director
+    result[director[:name]] = gross_for_director(director)
+    
 
 end
